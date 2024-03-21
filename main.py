f4cfab6e269a2154c4dda479ada635567c2be14c
Arsen Vaisov <vaisovarsen87@gmail.com>
2024-03-21 11:55:14 +0200

    Added points and spaces. Syntax fix

--- main.py
+++ main.py
@@ -10,23 +10,22 @@ def get_film_info(id):
 
     print(f"Персонажі: ")
     for index, character in enumerate(characters, 1):
-        print(f'{index} {character.name} з планети {character.get_homeworld().name}')
+        print(f'{index}.  {character.name} з планети {character.get_homeworld().name}')
 
     print(f"Транспортні засоби: ")
 
     for index, vehicle in enumerate(vehicles, 1):
-        print(f'{index} {vehicle.name}')
+        print(f'{index}.  {vehicle.name}')
 
     print(f"Космічні кораблі: ")
     for index, starship in enumerate(starships, 1):
-        print(f'{index} {starship.name}')
+        print(f'{index}.  {starship.name}')
 
     print(f"Види істот: ")
     for index, specie in enumerate(species, 1):
-        print(f'{index} {specie.name}')
+        print(f'{index}.  {specie.name}')
 
 if __name__ == '__main__':
-    print(
-        "1:'A New Hope', 2:'The Empire Strikes Back', 3:'Return of the Jedi', 4:'The Phantom Menace', 5:'Attack of the Clones', 6:'Revenge of the Sith'.")
+    print("1:'A New Hope', 2:'The Empire Strikes Back', 3:'Return of the Jedi', 4:'The Phantom Menace', 5:'Attack of the Clones', 6:'Revenge of the Sith'.")
     film_id = input("Введіть ідентифікатор фільму: ")
     get_film_info(film_id)
