6ce7c6b8eebb27c997a59c0be0fbac8be54655a5
Arsen Vaisov <vaisovarsen87@gmail.com>
2024-03-21 11:00:46 +0200

    Added print of characters

---
+++ main.py
@@ -0,0 +1,18 @@
+import swapi
+def get_film_info(id):
+    film = swapi.get_film(film_id)
+    characters = film.get_characters().items
+    vehicles = film.get_vehicles().items
+    starships = film.get_starships().items
+    species = film.get_species().items
+
+    print(f'Фільм: {film.title}')
+
+    print(f"Персонажі: ")
+    for index, character in enumerate(characters, 1):
+        print(f'{index} {character.name} з планети {character.get_homeworld().name}')
+
+
+if __name__ == '__main__':
+    film_id = input("Введіть ідентифікатор фільму: ")
+    get_film_info(film_id)
